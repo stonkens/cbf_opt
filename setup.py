5ef79fee3354440585ae209dc9962da145f5d299
stonkens <sandertonkens@gmail.com>
2022-01-16 17:41:58 -0800

    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name="cbf_qp",
+    version="0.1.0",
+    description="CBF QP implementation in Python",
+    author="Sander Tonkens",
+    author_email="sandertonkens@gmail.com",
+    python_requires=">=3.6",
+)
