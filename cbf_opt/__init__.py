20f5d40654608429ae7c491d47c2ae34387b2352
Sander Tonkens <sandertonkens@gmail.com>
2022-03-22 11:28:50 -0700

    Changes to version numbering

---
+++ cbf_opt/__init__.py
@@ -0,0 +1,17 @@
+from cbf_opt.dynamics import Dynamics, ControlAffineDynamics
+from cbf_opt.cbf import (
+    CBF,
+    ControlAffineCBF,
+    ImplicitCBF,
+    ControlAffineImplicitCBF,
+    BackupController,
+)
+from cbf_opt.asif import (
+    ASIF,
+    ControlAffineASIF,
+    ImplicitASIF,
+    ImplicitControlAffineASIF,
+    TradeoffFilter,
+)
+
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
+import os
 import setuptools
+
+_CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def _get_version():
+    with open(os.path.join(_CURRENT_DIR, "cbf_opt", "__init__.py")) as f:
+        for line in f:
+            if line.startswith("__version__") and "=" in line:
+                version = line[line.find("=") + 1 :].strip(" '\"\n")
+                if version:
+                    return version
+        raise ValueError("`__version__` not defined in `cbf_opt/__init__.py`")
 
 
 setuptools.setup(
     name="cbf_opt",
-    version="0.1.0",
+    version=_get_version(),
     description="CBF QP implementation in Python",
     author="Sander Tonkens",
     author_email="sandertonkens@gmail.com",
